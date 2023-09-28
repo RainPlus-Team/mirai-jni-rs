c525cdf8c0943457370cf533a8b7e075ccaecf38
YUCLing <luotianyi@luotianyi.me>
2023-09-28 21:57:49 +0800

    add: bot configuration jni classes

--- rs/mirai-jni-rs/src/classes.rs
+++ rs/mirai-jni-rs/src/classes.rs
@@ -1,8 +1,15 @@
+use const_format::concatcp;
+
 pub const CLASS: &str = "java/lang/Class";
 pub const STRING: &str = "java/lang/String";
 
 pub const BOT_FACTORY: &str = "net/mamoe/mirai/BotFactory";
 pub const BOT_AUTHORIZATION: &str = "net/mamoe/mirai/auth/BotAuthorization";
+
+pub const BOT_CONFIGURATION: &str = "net/mamoe/mirai/utils/BotConfiguration";
+pub const BOT_CONFIGURATION_HEARTBEAT_STRATEGY: &str = concatcp!(BOT_CONFIGURATION, "$HeartbeatStrategy");
+pub const BOT_CONFIGURATION_MIRAI_PROTOCOL: &str = concatcp!(BOT_CONFIGURATION, "$MiraiProtocol");
+pub const BOT_CONFIGURATION_CONTACT_LIST_CACHE: &str = concatcp!(BOT_CONFIGURATION, "$ContactListCache");
 
 pub const BOT: &str = "net/mamoe/mirai/Bot";
 
@@ -20,6 +27,5 @@ pub const EXTERNAL_RESOURCE: &str = "net/mamoe/mirai/utils/ExternalResource";
 pub const HELPER: &str = "org/rainplus/mirai/loader/Helper";
 
 pub const BOT_LOGIN: &str = "org/rainplus/mirai/loader/api/BotLogin";
-pub const BOT_CONF: &str = "org/rainplus/mirai/loader/api/BotConfiguration";
 pub const BOT_ACTION: &str = "org/rainplus/mirai/loader/api/BotAction";
 pub const BOT_EVENT: &str = "org/rainplus/mirai/loader/api/BotEvent";
