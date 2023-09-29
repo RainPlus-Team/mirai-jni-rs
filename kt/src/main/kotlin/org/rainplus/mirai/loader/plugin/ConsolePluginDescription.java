e4b5c5d83e59f3429fc2b258e04289e4d84320de
YUCLing <luotianyi@luotianyi.me>
2023-09-29 11:39:46 +0800

    add: more description fields

--- kt/src/main/kotlin/org/rainplus/mirai/loader/plugin/ConsolePluginDescription.java
+++ kt/src/main/kotlin/org/rainplus/mirai/loader/plugin/ConsolePluginDescription.java
@@ -1,13 +1,31 @@
 package org.rainplus.mirai.loader.plugin;
 
+import net.mamoe.mirai.console.plugin.description.PluginDependency;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConsolePluginDescription {
     public final String id;
     public final String version;
     public final String name;
+    public String author = "";
+    public String info = "";
+
+    public Set<PluginDependency> dependencies = new HashSet<>();
 
     public ConsolePluginDescription(String id, String version, String name) {
         this.id = id;
         this.version = version;
         this.name = name;
+    }
+
+    public void dependsOn(String id, @Nullable String versionRequirement, boolean isOptional) {
+        dependencies.add(new PluginDependency(id, versionRequirement, isOptional));
+    }
+
+    public void dependsOn(String id, @Nullable String versionRequirement) {
+        dependsOn(id, versionRequirement, false);
     }
 }
