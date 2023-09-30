25efe1881a37bc7d98c5a096237130910ff18548
YUCLing <luotianyi@luotianyi.me>
2023-10-01 03:40:23 +0800

    refactor: internally expose JNIEnv refactor: move create bot to bot module

--- rs/mirai-jni-rs/src/model/env.rs
+++ rs/mirai-jni-rs/src/model/env.rs
@@ -1,8 +1,4 @@
-use jni::{JNIEnv, objects::JValueGen};
-
-use crate::classes;
-
-use super::{bot::{Bot, login::BotAuthorization}, JavaObject};
+use jni::JNIEnv;
 
 pub struct MiraiEnv<'a> {
     env: JNIEnv<'a>
@@ -13,32 +9,7 @@ impl<'a> MiraiEnv<'a> {
         MiraiEnv { env }
     }
 
-    pub fn create_bot<T>(&self, qq: i64, auth: T) -> Bot<'a>
-        where T: Into<BotAuthorization>
-    {
-        let mut env = unsafe { self.env.unsafe_clone() };
-        let auth: BotAuthorization = auth.into();
-
-        let class = env.find_class(classes::BOT_AUTHORIZATION).unwrap();
-        let j_auth =
-        match auth {
-            BotAuthorization::Password(password) => {
-                env.call_static_method(class, "byPassword", format!("(L{};)L{};", classes::STRING, classes::BOT_AUTHORIZATION), &[
-                    JValueGen::Object(jni_str!(env, password))
-                ]).unwrap()
-            },
-            BotAuthorization::QRCode => {
-                env.call_static_method(class, "byQRCode", format!("()L{};", classes::BOT_AUTHORIZATION), &[]).unwrap()
-            }
-        };
-
-        let class = env.find_class(classes::BOT_FACTORY).unwrap();
-        let inst = env.get_static_field(class, "INSTANCE", format!("L{}$INSTANCE;", classes::BOT_FACTORY)).unwrap().l().unwrap();
-        let bot = env.call_method(inst, "newBot", format!("(JL{};)L{};", classes::BOT_AUTHORIZATION, classes::BOT), &[
-            JValueGen::Long(qq),
-            j_auth.borrow()
-        ]).unwrap().l().unwrap();
-
-        JavaObject::new(&env, &bot).into()
+    pub(crate) fn get_env(&self) -> JNIEnv<'_> {
+        unsafe { self.env.unsafe_clone() } // should we make this fully public with unsafe? if so, do we really need MiraiEnv?
     }
 }
